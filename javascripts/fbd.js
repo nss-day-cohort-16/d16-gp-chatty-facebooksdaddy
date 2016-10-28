45cf283836c2d8354c57d7c31df79a1e7cd938c9
john pinkston <pinkstonjack2@gmail.com>
2016-10-28 10:13:36 -0500

    addNewMessag_function

--- javascripts/fbd.js
+++ javascripts/fbd.js
@@ -1,10 +1,24 @@
 "use strict";
 
 var Chatty = (function(Chatty) {
-	return {
+	var inputMessages = [];
+	var counter = 6;
+	
+		Chatty.addNewMessage = function( input) {
+			var Messages = {
+				message: input,
+				id: counter
+				};
+			inputMessages.push(Messages);
+			Chatty.loadNewMessage(input, counter);
+			Chatty.listenSingleMessage(counter);
+			counter++;
 
 	};
+		// console.log(inputMessages);
+		return Chatty;
 
 })(Chatty || {});
 
 console.log("fbd js", Chatty);
+// Chatty.addNewMessage( "apfijghaijfgaeirhg");
