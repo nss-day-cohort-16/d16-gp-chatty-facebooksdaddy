2645d869191248bc7bfee84847f8f8b3e3952f80
juliakimchung <julhkm@gmail.com>
2016-10-28 14:31:30 -0500

    pulling in json data

--- javascripts/data.js
+++ javascripts/data.js
@@ -1,10 +1,24 @@
 "use strict";
 
-var Chatty = (function(Chatty) {
-	return {
+var Chatty = (function(oldChatty) {
+	var  originalMessages = {};
+	oldChatty.getData = function () {
+	var loader = new XMLHttpRequest();
 
-	};
+      loader.open("GET","javascripts/chatty.JSON");
+      loader.send();
+      
+      loader.addEventListener("load", function () {
+      var originalMessages = JSON.parse(this.responseText);
+      console.log("original", originalMessages );
+      //callbackToInvoke(dinoCarnivores);
+      
+      
+				//callbackToInvoke(originalMessages);
+		//pass array to load original
+			});
+		}
+		return oldChatty;
 
 })(Chatty || {});
 
-console.log("data js", Chatty);
