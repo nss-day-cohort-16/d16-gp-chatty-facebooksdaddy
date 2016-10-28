d04595924264e1fb67bd46abad2c9917d503e5c2
Nathan T. Baker <nathantbaker@gmail.com>
2016-10-28 11:20:22 -0500

    add loadOriginalMessages function

--- javascripts/DOM.js
+++ javascripts/DOM.js
@@ -1,9 +1,33 @@
 "use strict";
 
 var Chatty = (function(Chatty) {
-	return {
 
-	};
+  // var testArray = ["I'm the first message.", "I'm the second message.", "I'm the third message.", "I'm the fourth message.", "I'm the fifth message."];
+
+  // - push html out. add ids message-1 through message-5 and delete-1 etc
+  Chatty.loadOriginalMessages = function (array) {
+    var targetElement = document.getElementById("container");
+    var string = "";
+    var counter = 1;
+      for (var i = 0; i < array.length; i++) {
+          string +=
+          `<div class="messageContainer">
+              <div class="messageText">
+                <p>${array[i]}</p>
+              </div>
+              <div class="deleteButton deleteButton-${counter}">
+                <button>Delete</button>
+              </div>
+            </div>
+          </div>`
+        ;
+        counter++;
+      }
+    targetElement.innerHTML = string;
+
+  };
+// Chatty.loadOriginalMessages(testArray);
+  return Chatty;
 
 })(Chatty || {});
 
